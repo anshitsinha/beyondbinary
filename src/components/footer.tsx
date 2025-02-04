a829b33fb560e14fbd80d8208400ca18c2c78ab8
Anshit <136147899+anshitsinha@users.noreply.github.com>
2025-02-04 20:09:24 +0530

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,22 +7,41 @@ import { Input } from "./ui/input";
 import { createSubscriber } from "@/lib/actions";
 import { useFormState } from "react-dom";
 import SubmitButton from "./SubmitButton";
+import { DotGothic16, Instrument_Serif } from "next/font/google";
+
+// Initialize fonts at the module scope
+const title = Instrument_Serif({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const binary = DotGothic16({
+  subsets: ["latin"],
+  weight: "400",
+});
 
 export default function Footer() {
   const initialState = { message: "", errors: {} };
   const [state, dispatch] = useFormState(createSubscriber, initialState);
   return (
-    <footer className="bg-gray-100 py-8 dark:bg-gray-800 mt-10">
-      <div className="container mx-auto px-4 md:px-6">
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-4">
+    <footer className="bg-muted py-8 mt-10">
+      <div className="  mx-auto w-full xl:w-[90%] px-3">
+        <div className="grid grid-cols-1 gap-8 md:grid-cols-[7fr_1fr_1fr]
+">
           <div className="space-y-4">
             <div className="flex items-center space-x-2">
-              <Icons.logo className="h-6 w-6" />
-              <span className="text-md font-semibold">Coding Jitsu</span>
+              <h1 className="text-6xl text-primary md:text-9xl ">
+                {" "}
+                <span className="">BEYOND </span>{" "}
+                <span
+                  className={`${binary.className} text-[3.3rem] md:text-[7.5rem] tracking-wide md:tracki `}
+                >
+                  B<span className="md:-mx-4 -mx-2">I</span>NARY
+                </span>{" "}
+                {/* <span className=" ">Binary</span> */}
+              </h1>
             </div>
-            <p className="text-gray-500 dark:text-gray-400 text-sm">
-              Stay Up to Date with the latest news and insights from our blog.
-            </p>
+            
             <div className="flex space-x-4">
               <a
                 href="https://twitter.com/w3tsadev"
@@ -30,7 +49,7 @@ export default function Footer() {
                 rel="noopener noreferrer"
                 aria-label="Twitter"
               >
-                <Icons.twitter className="h-6 w-6 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300" />
+                <Icons.twitter className="h-6 w-6 text-muted-foreground hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300" />
               </a>
               <a
                 href="https://github.com/w3tsadev"
@@ -38,18 +57,18 @@ export default function Footer() {
                 rel="noopener noreferrer"
                 aria-label="Github"
               >
-                <Icons.gitHub className="h-6 w-6 text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300" />
+                <Icons.gitHub className="h-6 w-6 text-muted-foreground hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300" />
               </a>
             </div>
           </div>
           <div className="space-y-4">
-            <h3 className="text-md font-semibold">Blog</h3>
+            <h3 className="text-md text-primary font-semibold">Blog</h3>
             <ul className="space-y-2 text-sm">
               {POSTS.map((post) => (
                 <li key={post.title}>
                   <Link
                     href={post.href}
-                    className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+                    className="text-muted-foreground hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
                   >
                     {post.title}
                   </Link>
@@ -58,12 +77,12 @@ export default function Footer() {
             </ul>
           </div>
           <div className="space-y-4">
-            <h3 className="text-md font-semibold">Links</h3>
+            <h3 className="text-md text-primary font-semibold">Links</h3>
             <ul className="space-y-2 text-sm">
               <li>
                 <a
                   href="mailto:w3tsadev@gmail.com"
-                  className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+                  className="text-muted-foreground hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
                 >
                   Contact
                 </a>
@@ -71,7 +90,7 @@ export default function Footer() {
               <li>
                 <Link
                   href="/terms-of-services"
-                  className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+                  className="text-muted-foreground hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
                 >
                   Terms of Services
                 </Link>
@@ -79,7 +98,7 @@ export default function Footer() {
               <li>
                 <Link
                   href="/privacy-policy"
-                  className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+                  className="text-muted-foreground hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
                 >
                   Privacy Policy
                 </Link>
@@ -87,54 +106,16 @@ export default function Footer() {
               <li>
                 <Link
                   href="/sitemap.xml"
-                  className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
+                  className="text-muted-foreground hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300"
                 >
                   Sitemap
                 </Link>
               </li>
             </ul>
           </div>
-          <div className="space-y-4">
-            <h3 className="text-md font-semibold">Newsletter</h3>
-            <p className="text-gray-500 dark:text-gray-400 text-sm">
-              Subscribe to our newsletter to stay up-to-date with the latest
-              news and updates.
-            </p>
-            <form action={dispatch}>
-              <div className="flex space-x-2">
-                <Input
-                  type="email"
-                  name="email"
-                  id="email"
-                  placeholder="Enter your email"
-                  className="flex-1"
-                  defaultValue=""
-                  aria-describedby="email-error"
-                />
-                <SubmitButton />
-              </div>
-              <div
-                id="email-error"
-                aria-label="polite"
-                aria-atomic="true"
-                className="px-1"
-              >
-                {state?.errors?.email && (
-                  <p
-                    key={state.errors.email[0]}
-                    className="text-xs text-red-500"
-                  >
-                    {state.errors.email[0]}
-                  </p>
-                )}
-                {!state?.errors?.email && (
-                  <p className="text-xs text-green-500">{state?.message}</p>
-                )}
-              </div>
-            </form>
-          </div>
+       
         </div>
-        <div className="mt-8 border-t border-gray-200 pt-4 text-center text-xs text-gray-500 dark:border-gray-700 dark:text-gray-400">
+        <div className="mt-8 border-t border-gray-200 pt-4 text-center text-xs text-muted-foreground dark:border-gray-700 dark:text-gray-400">
           &copy; 2024 Coding Jitsu. All rights reserved.
         </div>
       </div>
